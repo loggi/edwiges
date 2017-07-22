64b8539c62af8b016e4ae0c6683e9d8979227756
Gabriela Surita <gabsurita@gmail.com>
2017-07-22 01:10:59 -0300

    Prepare publishing

--- setup.py
+++ setup.py
@@ -18,20 +18,28 @@ with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
 
-setup(name='edwiges',
-    version=0.1,
-    description='edwiges',
+setup(
+    name='edwiges',
+    version='0.1.0',
+    description='A stupidly simple email REST service.',
     long_description=README,
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Framework :: Pylons",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
     ],
     keywords="web services",
-    author='',
-    author_email='',
-    url='',
+    author='Gabriela Surita',
+    author_email='gsurita@loggi.com',
+    url='https://github.com/loggi/edwiges',
+    licence='Apache Licence v2.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
